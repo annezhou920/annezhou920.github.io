e2400088c43302b542ed259c3776615d12d66dec
Anne Zhou <anne.zhou920@gmail.com>
2017-09-12 01:37:20 -0400

    overflow x added to main container

--- src/About.js
+++ src/About.js
@@ -10,34 +10,41 @@ export default class About extends Component {
         </header>
         <section className="content__wrapper">
           <main className="main__container">
-            <div className="blue__wrapper"></div>
-            <h4>
-              1|&#8193;<span className="span__text--blue">class</span>
-              <span className="span__text--black"> Person</span> &#123;
-            </h4>
-            <h4>
-              2|&#8193;&#8193;<span className="span__text--red">constructor</span> (
-              <span className="span__text--black">job, lovesDonuts</span>) &#123;
-            </h4>
-            <h4>
-              3|&#8193;&#8193;&#8193;<span className="span__text--blue">this</span>.job = job;
-            </h4>
-            <h4>
-              5|&#8193;&#8193;&#8193;<span className="span__text--blue">this</span>.lovesDonuts = lovesDonuts;
-            </h4>
-            <h4>6|&#8193;&#8193;}</h4>
-            <h4>7|&#8193;</h4>
-            <h4>8|&#8193;&#8193;<span className="span__text--red">codes</span>()&#123;</h4>
-            <h4>7|&#8193;}</h4>
-            <h4>8|&#8193;</h4>
-            <h4>
-              9|&#8193;<span className="span__text--blue">const </span>
-              <span className="span__text--black">Anne</span> =
-              <span className="span__text--blue"> new</span>
-              <span className="span__text--black"> Person</span>(
-              <span className="span__text--green">'Software Developer'</span>,
-              <span className="span__text--blue"> true</span>);
-            </h4>
+            <div className="vertical__line"></div>
+            <ol>
+              <li><h4>
+                 &#8193;<span className="span__text--blue">class</span>
+                <span className="span__text--black"> Person</span> &#123;
+              </h4></li>
+              <li><h4>
+                &#8193;&#8193;<span className="span__text--red">constructor</span> (
+                <span className="span__text--black">job, lovesDonuts</span>) &#123;
+              </h4></li>
+              <li><h4>
+                &#8193;&#8193;&#8193;<span className="span__text--blue">this</span>.job = job;
+              </h4></li>
+              <li><h4>
+                &#8193;&#8193;&#8193;<span className="span__text--blue">this</span>.lovesDonuts = lovesDonuts;
+              </h4></li>
+              <li><h4>&#8193;&#8193;}</h4></li>
+              <li><h4>&#8193;</h4></li>
+              <li><h4>&#8193;&#8193;<span className="span__text--red">codes</span>()&#123;</h4></li>
+              <li><h4>
+                &#8193;&#8193;&#8193;
+                <span className="span__text--blue">return this</span>.
+                <span className="span__text--red"><a href="/projects">clickMe()</a></span>;
+              </h4></li>
+              <li><h4>&#8193;&#8193;}</h4></li>
+              <li><h4>&#8193;}</h4></li>
+              <li><h4>
+                &#8193;<span className="span__text--blue">const </span>
+                <span className="span__text--black">Anne</span> =
+                <span className="span__text--blue"> new</span>
+                <span className="span__text--black"> Person</span>(
+                <span className="span__text--green">'Software Developer'</span>,
+                <span className="span__text--blue"> true</span>);
+              </h4></li>
+            </ol>
           </main>
           <div className="sidebar">
             <ul>

39f9ccc5fad2c97f087260d65a4bec42778276b8
Anne Zhou <anne.zhou920@gmail.com>
2018-01-04 02:07:29 -0500

    restyled

--- src/About.js
+++ src/About.js
@@ -6,14 +6,14 @@ export default class About extends Component {
     return (
       <div className="about">
         <header className="header">
-          <h1><code>hello world!</code></h1>
+          <h1><code>anne zhou</code></h1>
         </header>
         <section className="content__wrapper">
           <main className="main__container">
             <div className="vertical__line"></div>
             <ol>
               <li><h4>
-                 &#8193;<span className="span__text--blue">class</span>
+                &#8193;<span className="span__text--blue">class</span>
                 <span className="span__text--black"> Person</span> &#123;
               </h4></li>
               <li><h4>
@@ -27,26 +27,18 @@ export default class About extends Component {
                 &#8193;&#8193;&#8193;<span className="span__text--blue">this</span>.lovesDonuts = lovesDonuts;
               </h4></li>
               <li><h4>&#8193;&#8193;}</h4></li>
-              <li><h4>&#8193;</h4></li>
-              <li><h4>&#8193;&#8193;<span className="span__text--red">codes</span>()&#123;</h4></li>
-              <li><h4>
-                &#8193;&#8193;&#8193;
-                <span className="span__text--blue">return this</span>.
-                <span className="span__text--red"><a href="/projects">clickMe()</a></span>;
-              </h4></li>
-              <li><h4>&#8193;&#8193;}</h4></li>
               <li><h4>&#8193;}</h4></li>
               <li><h4>
                 &#8193;<span className="span__text--blue">const </span>
                 <span className="span__text--black">Anne</span> =
                 <span className="span__text--blue"> new</span>
                 <span className="span__text--black"> Person</span>(
-                <span className="span__text--green">'Software Developer'</span>,
+                <span className="span__text--green">'Frontend Developer'</span>,
                 <span className="span__text--blue"> true</span>);
               </h4></li>
             </ol>
           </main>
-          <div className="sidebar">
+          <div className="tools">
             <ul>
               <li>
                 <h1><span className="javascript">JavaScript</span></h1>
@@ -74,6 +66,9 @@ export default class About extends Component {
               </li>
               <li>
                 <h1><span className="postgresql">PostgreSQL</span></h1>
+              </li>
+              <li>
+                <h1><span className="angular">Angular</span></h1>
               </li>
             </ul>
           </div>
